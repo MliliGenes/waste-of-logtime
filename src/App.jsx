e7e145c5a685449fab248d9c8251a1f25162f75e
MliliGenes <saad.elm.77@gmail.com>
2025-07-11 00:23:26 +0100

    hmm

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,34 @@
 import setup from "./coalesce";
 import { useEffect } from "react";
 
+function Button({txt}){
+  return (
+    <button className="capitalize">
+      {txt}
+    </button>
+  )
+}
+
 function Header()
 {
   return (
-    <header>
-      <div className="text-6xl"><span className="text-primary">Chrono</span>Leet</div>
+    <header className="flex justify-between items-center ">
+      <div className="text-6xl font-light"><span className="text-primary">Chrono</span>Leet</div>
+      <Button txt="add User" />
     </header>
   )
 }
 
 function App() {
+  
   useEffect(() => {
     setup();
   });
+
   return (
-    <div id="app font-poppins font-thin">
+    <div id="app" className="font-poppins font-normal">
       <div className="content--canvas"></div>
-      <main className="container text-white ">
+      <main className="absolute mx-auto text-white w-screen px-10 mt-10">
         <Header />
       </main>
     </div>
